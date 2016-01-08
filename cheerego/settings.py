2c8a760fa2f668cf37eed85ef1739e9da3e990e2
Sammy Fung <sammy@sammy.hk>
2016-01-09 01:46:14 +0800

    fixed django loading issue

--- cheerego/settings.py
+++ cheerego/settings.py
@@ -1,11 +1,4 @@
 # Scrapy settings for cheerego project
-#
-# For simplicity, this file contains only the most important settings by
-# default. All the other settings are documented here:
-#
-#     http://doc.scrapy.org/topics/settings.html
-#
-
 BOT_NAME = 'cheerego'
 
 SPIDER_MODULES = ['cheerego.spiders']
@@ -14,13 +7,17 @@ NEWSPIDER_MODULE = 'cheerego.spiders'
 # Crawl responsibly by identifying yourself (and your website) on the user-agent
 #USER_AGENT = 'cheerego (+http://www.yourdomain.com)'
 
-import os, sys
-os.environ.setdefault("DJANGO_SETTINGS_MODULE","cheerdb.settings")
-path = os.path.join(os.path.dirname(__file__),'../cheerdb')
-sys.path.append(os.path.abspath(path))
-from django.conf import settings
+# Initialize Django web framework for data store
+# Use environment variable PYTHONPATH for abspath to Django project
+# and DJANGO_SETTINGS_MODULE for Settings filename of Django project
+try:
+    import django
+    django.setup()
+except ImportError:
+    # Allow to work without Django
+    pass
 
 ITEM_PIPELINES = {
-  'cheerego.pipelines.CheeregoPipeline': 300,
+    'cheerego.pipelines.CheeregoPipeline': 300,
 }
 
